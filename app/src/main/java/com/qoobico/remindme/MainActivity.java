3f64d34ad220f74db5525d12f86abae0e76b6ba8
zzZDenisZzz <CoRey2007@bigmir.net>
2018-02-08 21:30:49 +0200

    add content in NavigationView

--- app/src/main/java/com/qoobico/remindme/MainActivity.java
+++ app/src/main/java/com/qoobico/remindme/MainActivity.java
@@ -1,5 +1,6 @@
 package com.qoobico.remindme;
 
+import android.support.v4.widget.DrawerLayout;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.support.v7.widget.Toolbar;
@@ -7,15 +8,18 @@ import android.view.MenuItem;
 
 public class MainActivity extends AppCompatActivity {
 
-    private Toolbar toolbar;
+    private static final int LAYOUT = R.layout.activity_main;
 
+    private Toolbar toolbar;
+    private DrawerLayout drawerLayout;
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         setTheme(R.style.AppDefault);
         super.onCreate(savedInstanceState);
-        setContentView(R.layout.activity_main);
+        setContentView(LAYOUT);
 
         initToolBar();
+        initNavigationView();
     }
 
     private void initToolBar(){
@@ -29,6 +33,10 @@ public class MainActivity extends AppCompatActivity {
         });
 
         toolbar.inflateMenu(R.menu.menu);
+    }
+
+    private void initNavigationView() {
+        drawerLayout = findViewById(R.id.drawer_layout);
     }
 
 }
